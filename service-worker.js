627cc757c844d4fae7a58baaf767e7b2445df4d8
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-21 13:08:00 -0300

    Atualizar cache da correção

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "checkfrota-v4";
+const CACHE = "checkfrota-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
