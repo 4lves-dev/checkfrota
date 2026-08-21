2346c197cb9293e554807478ad92c72fdd813692
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-21 13:32:48 -0300

    Atualizar cache do aplicativo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v8";
-const ASSETS = ["./", "./index.html", "./styles.css?v=8", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v10";
+const ASSETS = ["./", "./index.html", "./styles.css?v=10", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
