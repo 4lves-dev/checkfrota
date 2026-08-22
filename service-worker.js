cafb63ab1a97e67619dc6464c14aa9fc582d5f10
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-22 09:52:03 -0300

    chore: atualizar cache do CheckFrota

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v10";
-const ASSETS = ["./", "./index.html", "./styles.css?v=10", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v11";
+const ASSETS = ["./", "./index.html", "./styles.css?v=11", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
@@ -7,3 +7,5 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
+
+
