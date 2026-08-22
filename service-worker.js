d5211330ea72ab6fd590d55030adf49284023bb9
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-22 11:43:08 -0300

    Inclui descrição completa na aprovação de manutenção

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v27";
-const ASSETS = ["./", "./index.html", "./aprovacao.html?v=27", "./styles.css?v=24", "./app.js?v=27", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v28";
+const ASSETS = ["./", "./index.html", "./aprovacao.html?v=28", "./styles.css?v=24", "./app.js?v=28", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
