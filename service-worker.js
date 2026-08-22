7c70a0bb2568fe5aaa1dadf72ca6c3b226e3f538
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-22 09:59:45 -0300

    chore: disponibilizar AprovaFrota offline

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v12";
-const ASSETS = ["./", "./index.html", "./styles.css?v=12", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v13";
+const ASSETS = ["./", "./index.html", "./aprovacao.html", "./styles.css?v=13", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
