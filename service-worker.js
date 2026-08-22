2a3b985735cdaa652407882f076bda90d1184351
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-22 11:32:39 -0300

    Renova cache da versão de aprovação

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v24";
-const ASSETS = ["./", "./index.html", "./aprovacao.html", "./styles.css?v=24", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v25";
+const ASSETS = ["./", "./index.html", "./aprovacao.html", "./styles.css?v=24", "./app.js?v=25", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
