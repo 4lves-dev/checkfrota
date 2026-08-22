bb2345969a46a666a9054280f87ad9e4901025a7
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-22 11:07:26 -0300

    chore: renovar cache de bases

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v16";
-const ASSETS = ["./", "./index.html", "./aprovacao.html", "./styles.css?v=16", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v17";
+const ASSETS = ["./", "./index.html", "./aprovacao.html", "./styles.css?v=17", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
