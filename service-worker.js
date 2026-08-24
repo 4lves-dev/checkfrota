319c29241e4d0e0eec215dd062396721168178ca
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-24 09:39:12 -0300

    Remove atalho de aprovação da tela do motorista

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "checkfrota-v38";
+const CACHE = "checkfrota-v39";
 const ASSETS = ["./", "./index.html", "./gestao.html", "./aprovacao.html?v=28", "./styles.css?v=24", "./supabase-config.js?v=1", "./app.js?v=38", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
