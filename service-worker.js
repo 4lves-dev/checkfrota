47c38850d695d14d3addb349aa6d647d59645fcc
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-24 10:32:20 -0300

    Atualiza cache para fotos dos chamados

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v45";
-const ASSETS = ["./", "./index.html", "./gestao.html", "./instalar-gestao.html", "./aprovacao.html?v=28", "./styles.css?v=24", "./supabase-config.js?v=1", "./app.js?v=41", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v46";
+const ASSETS = ["./", "./index.html", "./gestao.html", "./instalar-gestao.html", "./aprovacao.html?v=28", "./styles.css?v=24", "./supabase-config.js?v=1", "./app.js?v=46", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
