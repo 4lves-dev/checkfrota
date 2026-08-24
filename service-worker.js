d927059a0d894ed59564a42a349bcd6f053c8a96
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-24 07:26:39 -0300

    Remove painel de controle da versão dos motoristas

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "checkfrota-v32";
+const CACHE = "checkfrota-v33";
 const ASSETS = ["./", "./index.html", "./aprovacao.html?v=28", "./styles.css?v=24", "./app.js?v=31", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
