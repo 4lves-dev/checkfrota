928a61ff7fcf0258167a5b86c5910b5f531520ba
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-26 08:17:46 -0300

    Atualizar cache para aplicativo dos líderes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v73";
-const ASSETS = ["./", "./index.html", "./gestao.html", "./instalar-gestao.html", "./aprovacao.html?v=70", "./styles.css?v=28", "./supabase-config.js?v=1", "./app.js?v=73", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v74";
+const ASSETS = ["./", "./index.html", "./gestao.html", "./lider.html", "./instalar-gestao.html", "./instalar-lider.html", "./aprovacao.html?v=70", "./styles.css?v=28", "./supabase-config.js?v=1", "./app.js?v=73", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
