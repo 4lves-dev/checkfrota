a6c725027177677ffe2ff5e265e7e58913bda874
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-26 15:11:53 -0300

    Completa matrículas dos motoristas

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v89";
-const ASSETS = ["./", "./index.html", "./gestao.html", "./lider.html?v=84", "./instalar-gestao.html", "./instalar-lider.html", "./aprovacao.html?v=84", "./styles.css?v=89", "./supabase-config.js?v=1", "./app.js?v=89", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v90";
+const ASSETS = ["./", "./index.html", "./gestao.html", "./lider.html?v=84", "./instalar-gestao.html", "./instalar-lider.html", "./aprovacao.html?v=84", "./styles.css?v=90", "./supabase-config.js?v=1", "./app.js?v=90", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
