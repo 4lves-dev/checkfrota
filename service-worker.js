0844e9ad7510caa2c2938c9abf9ae08e0501ee94
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-08-28 15:49:47 -0300

    Rebuild management pending command center

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v137";
-const ASSETS = ["./", "./index.html", "./gestao.html?v=108", "./lider.html?v=129", "./instalar-gestao.html?v=125", "./instalar-lider.html?v=128", "./aprovacao.html?v=104", "./styles.css?v=109", "./supabase-config.js?v=1", "./app.js?v=137", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v139";
+const ASSETS = ["./", "./index.html", "./gestao.html?v=108", "./lider.html?v=129", "./instalar-gestao.html?v=125", "./instalar-lider.html?v=128", "./aprovacao.html?v=104", "./styles.css?v=110", "./supabase-config.js?v=1", "./app.js?v=139", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
