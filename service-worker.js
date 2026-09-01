d129417fffcf40e03bf3939b372e67f9c44a8d2b
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-09-01 17:10:49 -0300

    Corrige nova tentativa de permissão OneSignal (service-worker.js)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "checkfrota-v152";
-const ASSETS = ["./", "./index.html", "./gestao.html?v=152", "./lider.html?v=152", "./instalar-gestao.html?v=152", "./instalar-lider.html?v=152", "./aprovacao.html?v=152", "./styles.css?v=152", "./supabase-config.js?v=1", "./onesignal.js?v=152", "./app.js?v=152", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "checkfrota-v153";
+const ASSETS = ["./", "./index.html", "./gestao.html?v=153", "./lider.html?v=153", "./instalar-gestao.html?v=153", "./instalar-lider.html?v=153", "./aprovacao.html?v=153", "./styles.css?v=152", "./supabase-config.js?v=1", "./onesignal.js?v=153", "./app.js?v=153", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
