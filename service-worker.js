cb95c05a71612dab853f1d141f1bd2847f6243c2
4lves-dev <71532139+4lves-dev@users.noreply.github.com>
2026-09-03 23:22:43 -0300

    Renova cache PWA para a correção Master v172

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "checkfrota-v171";
+const CACHE = "checkfrota-v172";
 const ASSETS = ["./", "./index.html", "./gestao.html?v=172", "./lider.html?v=172", "./instalar-gestao.html?v=172", "./instalar-lider.html?v=172", "./aprovacao.html?v=172", "./styles.css?v=172", "./supabase-config.js?v=172", "./onesignal.js?v=172", "./app.js?v=172", "./manifest.webmanifest", "./gestao-manifest.webmanifest", "./lider-manifest.webmanifest", "./icons/icon.svg", "./version.json"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
